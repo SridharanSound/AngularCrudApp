ccf392b40af6b3df79a9282f4c37adc27d4ec54a
Sri <sridharans769@gmail.com>
2022-01-10 15:14:21 +0200

    initial commit

---
+++ PaymentAPI/PaymentAPI/PaymentAPI/Migrations/20220107170321_Fix Model.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentAPI.Migrations
+{
+    public partial class FixModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpirationDate",
+                table: "PaymentDetails",
+                type: "nvarchar(5)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpirationDate",
+                table: "PaymentDetails",
+                type: "nvarchar(5)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldNullable: true);
+        }
+    }
+}
